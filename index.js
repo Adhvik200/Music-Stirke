a765cfe653246304e7fb4d9451e016cb53c8ff32
Adhvik200 <adhviksharma00@gmail.com>
2021-07-30 14:42:38 +0530

    Music_Bot

--- index.js
+++ index.js
@@ -1,74 +1,41 @@
-const DisTube = require("distube")
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const fs = require("fs")
-const config = require("./config.json")
-const mySecret = process.env['TOKEN']
-const keepAlive = require('./server.js')
+const fs = require('fs');
+const discord = require('discord.js');
 
+const client = new discord.Client({ disableMentions: 'everyone' });
 
-client.config = require("./config.json")
-client.distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true, leaveOnFinish: true })
-client.commands = new Discord.Collection()
-client.aliases = new Discord.Collection()
-client.emotes = config.emoji
+const { Player } = require('discord-player');
 
-fs.readdir("./commands/", (err, files) => {
-    if (err) return console.log("Could not find any commands!")
-    const jsFiles = files.filter(f => f.split(".").pop() === "js")
-    if (jsFiles.length <= 0) return console.log("Could not find any commands!")
-    jsFiles.forEach(file => {
-        const cmd = require(`./commands/${file}`)
-        console.log(`Loaded ${file}`)
-        client.commands.set(cmd.name, cmd)
-        if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
-    })
-})
+const token = require(process.env.token);
 
-client.on("ready", () => {
-    console.log(`${client.user.tag} is ready to play music.`)
-    const server = client.voice.connections.size
-    client.user.setActivity({ type: "PLAYING", name: `music on ${server} servers` })
-})
+client.player = new Player(client);
+client.config = require('./config/bot');
+client.emotes = client.config.emojis;
+client.filters = client.config.filters;
+client.commands = new discord.Collection();
 
-client.on("message", async message => {
-    const prefix = config.prefix
-    if (!message.content.startsWith(prefix)) return
-    const args = message.content.slice(prefix.length).trim().split(/ +/g)
-    const command = args.shift().toLowerCase()
-    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
-    if (!cmd) return
-    if (cmd.inVoiceChannel && !message.member.voice.channel) return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
-    try {
-        cmd.run(client, message, args)
-    } catch (e) {
-        console.error(e)
-        message.reply(`Error: ${e}`)
-    }
-})
+fs.readdirSync('./commands').forEach(dirs => {
+    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
 
-const status = queue => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode === 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``
-client.distube
-    .on("playSong", (message, queue, song) => message.channel.send(
-        `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
-    ))
-    .on("addSong", (message, queue, song) => message.channel.send(
-        `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
-    ))
-    .on("playList", (message, queue, playlist, song) => message.channel.send(
-        `${client.emotes.play} | Play \`${playlist.title}\` playlist (${playlist.total_items} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
-    ))
-    .on("addList", (message, queue, playlist) => message.channel.send(
-        `${client.emotes.success} | Added \`${playlist.title}\` playlist (${playlist.total_items} songs) to queue\n${status(queue)}`
-    ))
-    // DisTubeOptions.searchSongs = true
-    .on("searchResult", (message, result) => {
-        let i = 0
-        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`)
-    })
-    // DisTubeOptions.searchSongs = true
-    .on("searchCancel", message => message.channel.send(`${client.emotes.error} | Searching canceled`))
-    .on("error", (message, err) => message.channel.send(`${client.emotes.error} | An error encountered: ${err}`))
+    for (const file of commands) {
+        const command = require(`./commands/${dirs}/${file}`);
+        console.log(`Loading command ${file}`);
+        client.commands.set(command.name.toLowerCase(), command);
+    };
+});
 
-keepAlive();
-client.login(mySecret)
+const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+
+for (const file of events) {
+    console.log(`Loading discord.js event ${file}`);
+    const event = require(`./events/${file}`);
+    client.on(file.split(".")[0], event.bind(null, client));
+};
+
+for (const file of player) {
+    console.log(`Loading discord-player event ${file}`);
+    const event = require(`./player/${file}`);
+    client.player.on(file.split(".")[0], event.bind(null, client));
+};
+
+client.login(token);
