eef0b6a029da52c0d65617f9a823efc8954a7615
Adhvik200 <adhviksharma00@gmail.com>
2021-08-13 16:23:27 +0530

    Added customisable prefix

--- index.js
+++ index.js
@@ -37,4 +37,41 @@ for (const file of player) {
     client.player.on(file.split(".")[0], event.bind(null, client));
 };
 
+const mongoose = require('mongoose');
+const prefix = require('./models/prefix');
+
+mongoose.connect('MONGODB_URL', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+client.on('message', async(message) => {
+    if (message.author.bot) return;
+
+    //Getting the data from the model
+    const data = await prefix.findOne({
+        GuildID: message.guild.id
+    });
+
+    const messageArray = message.content.split(' ');
+    const cmd = messageArray[0];
+    const args = messageArray.slice(1);
+
+    //If there was a data, use the database prefix BUT if there is no data, use the default prefix which you have to set!
+    if (data) {
+        const prefix = data.Prefix;
+
+        if (!message.content.startsWith(prefix)) return;
+        const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
+        commandfile.run(client, message, args);
+    } else if (!data) {
+        //set the default prefix here
+        const prefix = "m/";
+
+        if (!message.content.startsWith(prefix)) return;
+        const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
+        commandfile.run(client, message, args);
+    }
+})
+
 client.login(process.env.token);
